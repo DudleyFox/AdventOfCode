82e25b5c249800c066fee46335064c24c483c682
Dudley Fox <dudley.fox@gmail.com>
2022-12-09 22:09:52 -0600

    2022 Day 9 part 2

--- 2022/Day_09/solutionPart2.py
+++ 2022/Day_09/solutionPart2.py
@@ -1,4 +1,5 @@
 import itertools
+# import gridDebugger as gdb
 
 # got 2343, but it was too high
 # That's not the right answer; your answer is too high.
@@ -11,17 +12,17 @@ visited = set()
 
 def readDirections():
     with open('input.txt', 'r') as f:
-        return [x for x in f.readlines()]
+        return [x.strip() for x in f.readlines()]
 
 def moveHead(hPos, c):
     d, i = c.split(' ')
     i = int(i)
     if d == 'U':
         for x in range(1, i+1):
-            yield (hPos[0], hPos[1] + x)
+            yield (hPos[0], hPos[1] - x)
     if d == 'D':
         for x in range(1, i+1):
-            yield (hPos[0], hPos[1] - x)
+            yield (hPos[0], hPos[1] + x)
     if d == 'L':
         for x in range(1, i+1):
             yield (hPos[0] - x, hPos[1])
@@ -30,6 +31,13 @@ def moveHead(hPos, c):
             yield (hPos[0] + x, hPos[1])
     
     return
+
+def limit(i):
+    if i == 2:
+        return 1
+    if i == -2:
+        return -1
+    return i
     
 def moveTail(tPos, hPos):
     x1, y1 = hPos
@@ -38,25 +46,25 @@ def moveTail(tPos, hPos):
     dy = y1 - y2
     mx = 0
     my = 0
-    if abs(dx) > 1 and dy == 0:
+    if abs(dx) > 1:
         mx = dx > 0 and 1 or -1
-    if abs(dy) > 1 and dx == 0:
+        my = limit(dy)
+    if abs(dy) > 1:
         my = dy > 0 and 1 or -1
-    if abs(dx) > 1 and dy != 0:
-        mx = dx > 0 and 1 or -1
-        my = dy
-    if abs(dy) > 1 and dx != 0:
-        my = dy > 0 and 1 or -1
-        mx = dx
+        mx = limit(dx)
     t = (tPos[0] + mx, tPos[1] + my)
+    # print(f'{hPos}:{tPos}->{t}')
     return t     
 
 
 def followDirections(directions, visited):
-    hPos = (0, 0)
-    tails = list(itertools.repeat((0, 0), 9))
+    hPos = (11, 15)
+    start = hPos
+    tails = list(itertools.repeat((11,15), 9))
     visited.add(hPos)
     for d in directions:
+        # gdb.drawGrid(hPos, tails, start)
+        # print(d)
         for h in moveHead(hPos, d):
             hPos = h
             for i in range(len(tails)):
@@ -66,10 +74,12 @@ def followDirections(directions, visited):
                     c = tails[i-1]
                 tails[i] = moveTail(tails[i], c)
             visited.add(tails[-1])
+    #         if d == 'U 8':
+    #             gdb.drawGrid(hPos, tails, start)
+    # gdb.drawGrid(hPos, tails, start)
 
 
 if __name__ == '__main__':
     directions = readDirections()
     followDirections(directions, visited)
-    
     print(len(visited))
