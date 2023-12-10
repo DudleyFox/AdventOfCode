9a3316c9a853b30c84bcbd59c064d76de07d6421
Dudley Fox <dudley.fox@gmail.com>
2023-12-09 22:13:54 -0600

    Day 9 parts 1 and 2

---
+++ 2023/Day_09/solution2.py
@@ -0,0 +1,29 @@
+import sys
+
+def readSequences(fileName):
+    with open(fileName, 'r') as f:
+        for x in f.readlines():
+            l = x.strip()
+            if l != '':
+                yield [int(x) for x in l.split(' ')]
+
+
+def getNextNumber(sequence):
+    print(sequence)
+    if sequence[-1] == 0:
+        return 0
+    last = sequence[0]
+    first = last
+    newSeq = []
+    for x in sequence[1:]:
+        newSeq.append(x - last)
+        last = x
+    return first - getNextNumber(newSeq)
+
+if __name__ == "__main__":
+    sequences = readSequences(sys.argv[1])
+    lasts = []
+    for s in sequences:
+        lasts.append(getNextNumber(s))
+    print(sum(lasts))
+
