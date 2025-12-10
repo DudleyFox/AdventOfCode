d5352f81853c61264eb3f2f45100beacffb34ea5
fox <dudley.fox@gmail.com>
2025-12-09 21:41:58 -0600

    Day 9 partial of part 2

--- 2025/Day_09/solutionPart2.py
+++ 2025/Day_09/solutionPart2.py
@@ -1,15 +1,17 @@
 import sys
 import math
 
-# first guess: 4442262383
-# second guess: 4763932976
+# first guess: 4613357005 too high
+
 def readPoints(filename):
     points = []
     with open(filename,'r') as f:
         for l in f.readlines():
             line = l.strip()
             x,y = line.split(",")
-            yield(int(x),int(y))
+            x = int(x)
+            y = int(y)
+            yield(x,y)
 
 
 def calculateArea(p1, p2):
@@ -33,29 +35,77 @@ def getAreaList(points):
     areaList.sort(key=lambda x: x[0], reverse=True)
     return areaList
 
-def testPoint(polygonPoint, pointToTest):
-    x,y = polygonPoint
-    tX,tY = pointToTest
-    if y
-
-def isInPolygon(points, p1, p2):
+def addLine(pointSet, p1, p2):
+    pointSet.add(p1)
+    pointSet.add(p2)
     x1, y1 = p1
     x2, y2 = p2
-    x3, y3 = x1, y2
-    x4, y4 = x2, y1
-    
+    if x1 == x2:
+        if y1 < y2:
+            for j in range(y1, y2+1):
+                pointSet.add((x1,j))
+        else:
+            for j in range(y2, y1+1):
+                pointSet.add((x1,j))
+    else:
+        if x1 < x2:
+            for j in range(x1, x2+1):
+                pointSet.add((j,y1))
+        else:
+            for j in range(x2, x1+1):
+                pointSet.add((j,y1))
 
-    for p in points:
-        x,y=p
+def buildPolygonPointSet(points):
+    pointSet = set()
+    firstPoint = points[0]
+    lastPoint = points[-1]
+    for i in range(len(points) - 1):
+        p1 = points[i]
+        p2 = points[i+1]
+        addLine(pointSet, p1, p2)
+        # print("Added:", p1, p2, pointSet)
+    addLine(pointSet, lastPoint, firstPoint)
+    return pointSet
 
+def fillPolygon(pointSet):
+    pass
+
+def isPointInPolygon(pointSet, p):
+    # print("Testing:", p)
+    if p in pointSet:
+        return True
+    x,y = p
+    xPoints = [p for p in pointSet if p[0] == x]
+    yPoints = [p for p in pointSet if p[1] == y]
+    # print(p, xPoints, yPoints)
+    oneAbove = len([p for p in xPoints if p[1] <= y]) > 0
+    oneBelow = len([p for p in xPoints if p[1] >= y]) > 0
+    oneLeft = len([p for p in yPoints if p[0] <= x]) > 0
+    oneRight = len([p for p in yPoints if p[0] >= x]) > 0
+    # print("Above:", oneAbove, "Below:", oneBelow, "Right:", oneRight, "Left:", oneLeft)
+    return oneAbove and oneBelow and oneLeft and oneRight
+
+def isInPolygon(pointSet, p1,p2):
+    # print("*************\nTesting", p1, p2)
+    p3 = (p1[0], p2[1])
+    p4 = (p2[0], p1[1])
+    toTest = [p1,p2,p3,p4]
+    for p in toTest:
+        if not isPointInPolygon(pointSet, p):
+            return False
         
-
+    # print(pointSet)
+    return True
            
 if __name__ == "__main__":
     points = list(readPoints(sys.argv[1]))
     areas = getAreaList(points)
+    pointSet = buildPolygonPointSet(points)
     for a in areas:
-        rectPoints = getRectablePoints(a[1],a[2])
+        area, p1, p2 = a
+        if isInPolygon(pointSet, p1, p2):
+            print(area)
+            exit(0)
 
     
 
